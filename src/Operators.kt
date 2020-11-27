38e592fc6373b76e77c837173a30283792646586
Aditya Pratap <adityapratap7344@gmail.com>
2020-11-28 00:13:54 +0530

    learnt comparison operators

--- src/Operators.kt
+++ src/Operators.kt
@@ -15,7 +15,26 @@ fun main() {
     val num1 = 24.0
     val num2 = 54
     val divResult = num2 / num1
-    print("\nDivision Result: $divResult")
+    println("\nDivision Result: $divResult")
 
+    // Comparison operators (==, !=, <, >, <=, >=)
+    val val1 = 5
+    val val2 = 3
+    val isEqual = val1 == val2
+    println("isEqual: $isEqual")
 
+    val isNotEqual = val1 != val2
+    println("isNotEqual: $isNotEqual")
+
+    val isSmaller = val1 <= val2
+    println("isSmaller: $isSmaller")
+
+    val isGreater = val1 >= val2
+    println("isGreater: $isGreater")
+
+    val isStrictlySmaller = val1 < val2
+    println("isStrictlySmaller: $isStrictlySmaller")
+
+    val isStrictlyGreater = val1 > val2
+    println("isStrictlyGreater: $isStrictlyGreater")
 }
