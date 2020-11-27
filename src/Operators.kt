a2dd29ff6480216614261bc2c7755ff3c1cf31aa
Aditya Pratap <adityapratap7344@gmail.com>
2020-11-27 23:57:38 +0530

    learnt arithmetic operators

---
+++ src/Operators.kt
@@ -0,0 +1,21 @@
+fun main() {
+    // Arithmetic operators (+, -, *, /, %)
+    var result = 5 + 3
+    print("Result: $result")
+
+    result /= 2
+    print("\nResult2: $result")
+
+    result *= 4
+    print("\nResult3: $result")
+
+    val remainder = 15 % 2
+    print("\nRemainder: $remainder")
+
+    val num1 = 24.0
+    val num2 = 54
+    val divResult = num2 / num1
+    print("\nDivision Result: $divResult")
+
+
+}
