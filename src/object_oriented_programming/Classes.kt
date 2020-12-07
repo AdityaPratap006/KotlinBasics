1c3e40a52fa3d736771d27e876e78f1a75af9500
Aditya Pratap <adityapratap7344@gmail.com>
2020-12-08 03:23:15 +0530

    learnt about member variables and functions, as well as secondary constructors!

--- src/object_oriented_programming/Classes.kt
+++ src/object_oriented_programming/Classes.kt
@@ -1,17 +1,38 @@
 package object_oriented_programming
 
 fun main() {
-    val johnWick = Person("John", "Wick")
-    val aditya = Person()
-    val adityaRathore = Person(lastName = "Rathore")
+    val johnWick = Person("John", "Wick", 45)
+    val aditya = Person(age = 20)
+//    val adityaRathore = Person(lastName = "Rathore")
+    johnWick.stateHobby()
+    johnWick.hobby = "kill people!"
+    johnWick.stateHobby()
+
+    aditya.hobby = "play pc games!"
+    aditya.stateHobby()
 }
 
 class Person(firstName: String = "Aditya", lastName: String = "Pratap") {
 
+    // Member Variables - Properties
+    var age: Int? = null
+    var hobby: String = "watch Netflix"
+    var firstName: String? = null
     // Initializer Block
     init {
+        this.firstName = firstName
         println("Created a Person named $firstName $lastName!")
     }
 
+    // Member - Secondary Constructor
+    constructor(firstName: String = "Aditya", lastName: String = "Pratap", age: Int) : this(firstName, lastName) {
+        this.age  = age
+        println("$firstName $lastName is ${this.age} years old")
+    }
+
+    // Member Functions - Methods
+    fun stateHobby() {
+        println("$firstName's hobby is $hobby")
+    }
 
 }
